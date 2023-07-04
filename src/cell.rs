ad644ed78bc9dd512226195bda4452a032a53ebb
Olivia Palmu <oliviaspalmu@gmail.com>
2023-07-04 13:14:04 +0300

    Add docs

--- src/cell.rs
+++ src/cell.rs
@@ -1,32 +1,51 @@
-//! Module responsible for formatting black & white bitmaps into Unicode braille characters
+//! Module responsible for formatting black & white bitmaps into Unicode braille characters.
+//!
+//! See the [Cell] documentation for more.
 
 use crate::color::Color;
 
+/// The unicode scalar value for the first ("empty") braille codepoint.
 pub const BRAILLE_BASE_CODEPOINT: u32 = 0x2800;
+/// The number of bytes required to encode a braille unicode character into utf-8. This is a constant value,
+/// because the characters have codepoints between `U+0800` and `U+FFFF`.
 pub const BRAILLE_UTF8_BYTES: usize = 3;
-// Pixel format
-// 0 1
-// 2 3
-// 4 5
-// 6 7
-//
-// Offset format
-// 0 3
-// 1 4
-// 2 5
-// 6 7
 
+/// An offset into a unicode block with its bits permuted. More specifically,
+/// its bits follow the following format:
+/// ```txt
+/// 0 1
+/// 2 3
+/// 4 5
+/// 6 7
+/// ```
+///
+/// That is, the top left pixel of a cell is stored into the 0th bit, the top right pixel is the 1st
+/// bit, and so on.
+///
+/// On the contrary, the Unicode specification for braille characters provides the following format:
+/// ```txt
+/// 0 3
+/// 1 4
+/// 2 5
+/// 6 7
+/// ```
+/// That is, the braille character with the top left and top right dots set is encoded as an 8-bit offset
+/// from [BRAILLE_BASE_CODEPOINT] with the 0th and 3rd bits set, i.e. `0b1001`.
 #[derive(Clone, Copy, Default)]
 pub struct Cell {
+    /// The internal storage bits.
     pub bits: u8,
     pub fg: Color,
     pub bg: Color,
 }
 
 impl Cell {
+    /// A cell is exactly 2 pixels wide, since it consists of one braille character.
     pub const PIXEL_WIDTH: usize = 2;
+    /// A cell is exactly 4 pixels tall, since it consists of one braille character.
     pub const PIXEL_HEIGHT: usize = 4;
 
+    /// Create a new cell with the specified bits.
     pub fn new(bits: u8) -> Self {
         Self {
             bits,
@@ -35,6 +54,7 @@ impl Cell {
         }
     }
 
+    /// Computes the Unicode codepoint offset format of the braille character.
     pub const fn braille_offset(self) -> u8 {
         (self.bits & 0b11100001)
             | ((self.bits & 0b10) << 2)
@@ -49,9 +69,12 @@ impl Cell {
         // | ((offset & 0b10000) >> 1)
     }
 
-    // The braille character block contains only 3-byte utf-8 characters.
-    // Since we can assert the range of code points here, the optimizer can take some shortcuts.
+    /// Encodes the cell as a sequence of UTF-8 bytes representing
+    /// its braille encoded character.
     pub fn to_braille_utf8(self) -> [u8; BRAILLE_UTF8_BYTES] {
+        // The braille character block contains only 3-byte utf-8 characters.
+        // Since we can assert the range of code points here, the optimizer can take some shortcuts and,
+        // for example, elide this `unwrap()` operation.
         let c = char::from_u32(BRAILLE_BASE_CODEPOINT + self.braille_offset() as u32).unwrap();
         let mut b = [0; BRAILLE_UTF8_BYTES];
         c.encode_utf8(&mut b);
@@ -69,5 +92,18 @@ mod tests {
     fn unique_offset() {
         let map: HashSet<_> = (0u8..=255).map(|n| Cell::new(n).braille_offset()).collect();
         assert_eq!(map.len(), 256)
+    }
+
+    #[test]
+    fn correct_braille() {
+        assert_eq!(Cell::new(0).to_braille_utf8(), [226, 160, 128]);
+        assert_eq!(Cell::new(1).to_braille_utf8(), [226, 160, 129]);
+        assert_eq!(Cell::new(2).to_braille_utf8(), [226, 160, 136]);
+        assert_eq!(Cell::new(4).to_braille_utf8(), [226, 160, 130]);
+        assert_eq!(Cell::new(8).to_braille_utf8(), [226, 160, 144]);
+        assert_eq!(Cell::new(16).to_braille_utf8(), [226, 160, 132]);
+        assert_eq!(Cell::new(32).to_braille_utf8(), [226, 160, 160]);
+        assert_eq!(Cell::new(64).to_braille_utf8(), [226, 161, 128]);
+        assert_eq!(Cell::new(128).to_braille_utf8(), [226, 162, 128]);
     }
 }
--- src/screen.rs
+++ src/screen.rs
@@ -1,15 +1,32 @@
+//! Module for writing output to the screen.
+//! Contains the [Screen] type and its public interface.
+
 use crate::cell::{Cell, BRAILLE_UTF8_BYTES};
 
+/// Type used to write to the screen. Contains public methods
+/// to write pixels and sprites to the screen, as well as colors.
+///
+/// The point (0, 0) represents the top left pixel of the screen.
+///
+/// The [`Screen::rasterize`] method can be used to generate
+/// bytes that can be written to a terminal.
 pub struct Screen {
     cells: Vec<Cell>,
     width: usize,
     height: usize,
 }
 
+/// A blit type used to select the type of operation
+/// when writing to the screen. In the case of single pixels,
+/// this is used to determine whether the output pixel is
+/// set to 1, set to 0 or flipped.
 #[derive(Clone, Copy)]
 pub enum Blit {
-    Set,
+    /// Sets the output bits to 0
     Unset,
+    /// Set the output bits to 1
+    Set,
+    /// Flips the output bits, i.e. sets 1 to 0 and 0 to 1
     Toggle,
 }
 
@@ -19,6 +36,9 @@ fn div_ceil(a: usize, b: usize) -> usize {
 }
 
 impl Screen {
+    /// Create a new empty screen with the given dimensions in pixels.
+    /// The resulting width and height are rounded up to the nearest multiple of
+    /// [`Cell::PIXEL_WIDTH`] and [`Cell::PIXEL_HEIGHT`].
     pub fn new(width: usize, height: usize) -> Self {
         Self {
             cells: vec![
@@ -30,12 +50,21 @@ impl Screen {
         }
     }
 
+    /// Compute the height of the screen, in number of cells. This is a divisor of its pixel height.
     pub fn cell_height(&self) -> usize {
         div_ceil(self.height, Cell::PIXEL_HEIGHT)
     }
-
+    /// Compute the width of the screen, in number of cells. This is a divisor of its pixel width.
     pub fn cell_width(&self) -> usize {
         div_ceil(self.width, Cell::PIXEL_WIDTH)
+    }
+    /// Compute the height of the screen, in number of pixels. This is a multiple of its cell height.
+    pub fn pixel_height(&self) -> usize {
+        self.height
+    }
+    /// Compute the width of the screen, in number of pixels. This is a multiple of its cell width.
+    pub fn pixel_width(&self) -> usize {
+        self.width
     }
 
     fn pixel_index(&self, x: usize, y: usize) -> (usize, u8) {
@@ -46,7 +75,15 @@ impl Screen {
         (index, pixel)
     }
 
-    fn transform_pixel(&mut self, x: usize, y: usize, blit: Blit) {
+    #[allow(unused)]
+    fn pixel_at(&self, x: usize, y: usize) -> bool {
+        let (index, pixel) = self.pixel_index(x, y);
+        self.cells[index].bits & pixel != 0
+    }
+
+    /// Transforms the pixel value at the given coordinates with a generic given blitting strategy.
+    /// Returns `true` if a value was changed, and `false` if the given coordinate was out of bounds.
+    pub fn transform_pixel(&mut self, x: usize, y: usize, blit: Blit) -> bool {
         if x < self.width && y < self.height {
             let (index, pixel) = self.pixel_index(x, y);
             let orig = self.cells[index].bits;
@@ -55,24 +92,31 @@ impl Screen {
                 Blit::Unset => orig & !pixel,
                 Blit::Toggle => orig ^ pixel,
             };
+            true
+        } else {
+            false
         }
     }
 
-    pub fn pixel_at(&self, x: usize, y: usize) -> bool {
-        let (index, pixel) = self.pixel_index(x, y);
-        self.cells[index].bits & pixel != 0
-    }
-
-    /// Ignores out-of-bounds inputs
+    /// Sets the pixel value at the given coordinates to be the given value. If `value` is
+    /// `true`, sets the pixel value to be 1. Otherwise, sets it to 0.
+    ///
+    /// **Ignores** out-of-bounds input.
+    /// This may be preferred when drawing graphics that can partially clip off screen.
     pub fn set(&mut self, x: usize, y: usize, value: bool) {
         self.transform_pixel(x, y, if value { Blit::Set } else { Blit::Unset });
     }
 
-    /// Ignores out-of-bounds inputs
+    /// Flips the pixel value at the given coordinates to be 1.
+    ///
+    /// **Ignores** out-of-bounds input.
+    /// This may be preferred when drawing graphics that can partially clip off screen.
     pub fn toggle(&mut self, x: usize, y: usize) {
         self.transform_pixel(x, y, Blit::Toggle);
     }
 
+    /// Converts the screen to a utf-8 sequence of bytes that can be rendered in a terminal.
+    /// Includes newlines in its output.
     pub fn rasterize(&self) -> Vec<u8> {
         // additional + height given for newline chars
         let mut buf = vec![0; self.cells.len() * BRAILLE_UTF8_BYTES + self.cell_height()];
